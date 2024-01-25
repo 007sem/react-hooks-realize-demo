1089362dcd0d6addbfaec391a580a54a5a7db7b3
007sem <380204098@qq.com>
2024-01-25 16:57:45 +0800

    chore: 增加prettier格式化

--- src/app.jsx
+++ src/app.jsx
@@ -1,55 +1,51 @@
-import { root, useState, useReducer, useEffect } from './react'
+import { root, useState, useReducer, useEffect } from "./react";
 
-let t = null
+let t = null;
 
-function App (){
+function App() {
+	// const [ count, setCount ] = useState(0)
+	// const [ count2, setCount2 ] = useState(0)
 
-    // const [ count, setCount ] = useState(0)
-    // const [ count2, setCount2 ] = useState(0)
+	// function countReducer(count, { type, payload }) {
+	//     switch (type) {
+	//     case "PLUS":
+	//         return count + payload;
+	//     case "MINUS":
+	//         return count - payload;
+	//     case "TIMES":
+	//         return count * payload;
+	//     case "DIV":
+	//         return count / payload;
+	//     default:
+	//         return count;
+	//     }
+	// }
+	// const [count, dispatch] = useReducer(countReducer, 0);
 
-    
-    // function countReducer(count, { type, payload }) {
-    //     switch (type) {
-    //     case "PLUS":
-    //         return count + payload;
-    //     case "MINUS":
-    //         return count - payload;
-    //     case "TIMES":
-    //         return count * payload;
-    //     case "DIV":
-    //         return count / payload;
-    //     default:
-    //         return count;
-    //     }
-    // }
-    // const [count, dispatch] = useReducer(countReducer, 0);
+	const [count, setCount] = useState(0);
+	const [second, setSecond] = useState(0);
+	useEffect(() => {
+		console.log("useEffect");
 
-    
-    const [ count, setCount ] = useState(0)
-    const [second, setSecond] = useState(0);
-    useEffect(() => {
-        console.log('useEffect');
+		t = setInterval(() => {
+			setSecond((second) => second + 1);
+		}, 1000);
 
-        t = setInterval(() => {
-        setSecond((second) => second + 1);
-        }, 1000);
+		//页面卸载时
+		return () => {
+			clearInterval(t);
+			t = null;
+		};
+	}, []);
 
-        //页面卸载时
-        return () => {
-            clearInterval(t);
-            t = null;
-        };
-    }, []);
+	useEffect(() => {
+		setSecond(count);
+	}, [count]);
 
-    useEffect(() => {
-        setSecond(count);
-    }, [count]);
-    
-    return (
-        <div>
-
-            {/* -----------------useState--------------- */}
-            {/* <div>
+	return (
+		<div>
+			{/* -----------------useState--------------- */}
+			{/* <div>
                 <h1>{ count }</h1>
                 <button onClick={ () => setCount(count + 1) }>+</button>
                 <button onClick={ () => setCount(count - 1) }>-</button>
@@ -60,28 +56,25 @@ function App (){
                 <button onClick={ () => setCount2(count2 - 1) }>-</button>
             </div> */}
 
-            {/* -----------------useReducer--------------- */}
-            {/* <div>
+			{/* -----------------useReducer--------------- */}
+			{/* <div>
                 <h1>{ count }</h1>
                 <button onClick={() => dispatch({ type: "PLUS", payload: 1 })}>+</button>
                 <button onClick={() => dispatch({ type: "MINUS", payload: 1 })}>-</button>
             </div> */}
 
+			{/* -----------------useEffect--------------- */}
 
-            {/* -----------------useEffect--------------- */}
-
-            <div>
-                <h1>{ count }</h1>
-                <h1>{ second }s</h1>
-                <button onClick={ () => setCount(count + 1) }>+</button>
-                <button onClick={ () => setCount(count - 1) }>-</button>
-            </div>
-
-        </div>
-    )
+			<div>
+				<h1>{count}</h1>
+				<h1>{second}s</h1>
+				<button onClick={() => setCount(count + 1)}>+</button>
+				<button onClick={() => setCount(count - 1)}>-</button>
+			</div>
+		</div>
+	);
 }
 
 root.render(<App />);
 
-
-export default App
+export default App;
--- src/react.jsx
+++ src/react.jsx
@@ -1,102 +1,89 @@
 const { createRoot } = ReactDOM;
 export const root = createRoot(document.getElementById("app"));
 
-
-
 const states = [];
 const stateSetters = [];
 
-const effectDepArr = []
+const effectDepArr = [];
 
 let stateIndex = 0;
 let effectIndex = 0;
 
-function createState (initialState){
-    return states[stateIndex] ? states[stateIndex] : initialState;
+function createState(initialState) {
+  return states[stateIndex] ? states[stateIndex] : initialState;
 }
-function createStateSetter(index){
-    return (newState) =>{
-        if(typeof newState == "function"){
-            // 将函数执行并且将形参绑定到对应的值上
-            states[index] = newState(states[index]);
-        } else {
-            states[index] = newState; // 直接覆盖
-        }
-        render()
+function createStateSetter(index) {
+  return (newState) => {
+    if (typeof newState == "function") {
+      // 将函数执行并且将形参绑定到对应的值上
+      states[index] = newState(states[index]);
+    } else {
+      states[index] = newState; // 直接覆盖
     }
-}
-
-export const  useState = (initialState)=>{
-    states[stateIndex] = createState(initialState, stateIndex);
-
-    if(!stateSetters[stateIndex]){
-        stateSetters.push(createStateSetter(stateIndex));
-    }
-
-    const _state = states[stateIndex];
-    const _setter = stateSetters[stateIndex];
-
-    stateIndex++;
-
-    return [
-        _state,
-        _setter
-    ]
-
+    render();
+  };
 }
 
+export const useState = (initialState) => {
+  states[stateIndex] = createState(initialState, stateIndex);
 
-export function useReducer (reducer,initialState){
-    const [state, setState] = useState(initialState);
+  if (!stateSetters[stateIndex]) {
+    stateSetters.push(createStateSetter(stateIndex));
+  }
 
-    function dispatch(action){
-        const newState = reducer(state, action);
-        setState(newState);
-    }
+  const _state = states[stateIndex];
+  const _setter = stateSetters[stateIndex];
 
-    return [state, dispatch];
+  stateIndex++;
 
+  return [_state, _setter];
+};
+
+export function useReducer(reducer, initialState) {
+  const [state, setState] = useState(initialState);
+
+  function dispatch(action) {
+    const newState = reducer(state, action);
+    setState(newState);
+  }
+
+  return [state, dispatch];
 }
 
 export const useEffect = (cb, depArr) => {
-    if (typeof cb !== "function") {
-      throw new TypeError("callback must be a function");
-    }
-  
-    // 检查是否提供了依赖数组，如果未提供，则每次渲染都执行副作用回调
-    if (depArr === undefined) {
-      cb();
-    }
-    // 检查依赖数组的类型是否为数组
-    else if (!Array.isArray(depArr)) {
-      throw new TypeError("dependencies must be an array");
-    }
-    else {
-      // 检查依赖数组中的值是否发生了变化
-      // 注意，这里是通过将数组直接拿值的方式作比较来检查数组是否变化，而不是数据劫持响应式那一套  
-      const isChanged = effectDepArr[effectIndex]
-        ? depArr.some((dep, index) => dep !== effectDepArr[effectIndex][index])
-        : true;
-  
-      // 如果依赖数组中的值发生了变化，则执行副作用回调
-      isChanged && cb();
-  
-      // 将当前依赖数组保存到 effectDepArr 中
-      effectDepArr[effectIndex] = depArr;
-  
-      // 增加 effectIndex 的值，以便下一个 useEffect 调用使用不同的索引
-      effectIndex++;
-    }
-  };
+  if (typeof cb !== "function") {
+    throw new TypeError("callback must be a function");
+  }
 
+  // 检查是否提供了依赖数组，如果未提供，则每次渲染都执行副作用回调
+  if (depArr === undefined) {
+    cb();
+  }
+  // 检查依赖数组的类型是否为数组
+  else if (!Array.isArray(depArr)) {
+    throw new TypeError("dependencies must be an array");
+  } else {
+    // 检查依赖数组中的值是否发生了变化
+    // 注意，这里是通过将数组直接拿值的方式作比较来检查数组是否变化，而不是数据劫持响应式那一套
+    const isChanged = effectDepArr[effectIndex]
+      ? depArr.some((dep, index) => dep !== effectDepArr[effectIndex][index])
+      : true;
 
+    // 如果依赖数组中的值发生了变化，则执行副作用回调
+    isChanged && cb();
 
+    // 将当前依赖数组保存到 effectDepArr 中
+    effectDepArr[effectIndex] = depArr;
 
+    // 增加 effectIndex 的值，以便下一个 useEffect 调用使用不同的索引
+    effectIndex++;
+  }
+};
 
-async function render (){
-    const App = (await import("./app")).default;
+async function render() {
+  const App = (await import("./app")).default;
 
-    stateIndex = 0; // 重置状态索引
-    effectIndex = 0; // 重置副作用索引
-    root.render(<App />)
+  stateIndex = 0; // 重置状态索引
+  effectIndex = 0; // 重置副作用索引
+  root.render(<App />);
 }
